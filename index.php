f00c441eb0584a97814424e813488075323b6bfc
CallOBock <callobock.baralginovich@gmail.com>
2018-07-07 23:25:47 +0300

    Семантика расписана

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Эппл Shop - Главная страница</title>
+	<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+</head>
+<body>
+	<div class="wrapper_content">
+		<div class="main_content">
+			<header>
+				<ul class="social_list">
+					<li class="social_item">Вконтакте</li>
+					<li class="social_item">Facebook</li>
+					<li class="social_item">Twitter</li>
+				</ul>
+				<div class="addres">
+					<a href="#" class="map_address">
+						г. Москва, ул. Новая 246, офис 66
+					</a>
+				</div>
+				<div class="tel">
+					<a href="#" class="tel_address">
+						89021055850
+					</a>
+				</div>
+			</header>
+			<div class="">
+				<a href="#" class="logo"><span>Эппл Shop</span></a>
+				<form action="" method="post" class="search">
+					<input type="search" name="search" class="input" placeholder="Поиск">
+					<input type="submit" name="search_submit" class="input_submit">
+				</form>
+				<ul class="header_menu">
+					<li class="road_map">
+						<a href="" class="road_map link">Карта проезда</a>
+						<a href="" class="history_orders link">История заказов</a>
+						<a href="" class="cart link">Корзина</a>
+					</li>
+				</ul>
+			</div>
+			<aside class="sidebar">
+				<nav class="navigation">
+					<ul class="navigation_list">
+						<li class="naviagtion_link">Мобильные телефоны</li>
+						<li class="naviagtion_link">Планшеты</li>
+						<li class="naviagtion_link">Mр3 плееры</li>
+						<li class="naviagtion_link">Персональные компьютеры</li>
+						<li class="naviagtion_link">Ноутбуки</li>
+					</ul>
+				</nav>
+			</aside>
+			<section class="breadcrumbs">
+				<div class="container">
+					<ul class="breadcrumbs_list">
+						<li class="breadcrumbs_item"><a href="#" class="breadcrumbs_link">Главная</a></li>
+						<li class="breadcrumbs_item"><a href="#" class="breadcrumbs_link">Каталог</a></li>
+						<li class="breadcrumbs_item activ">Планшеты</li>
+					</ul>
+				</div>
+			</section>
+			<div class="product">
+				<section class="product_image">
+					<div class="image_wrap">
+						<div class="avatar_box">
+							<img src="" alt="Image product">
+						</div>
+					</div>
+					<div class="product_information">
+						<h2 class="product_name">Бла бла бла Название продукта со всеми свителками и перделками</h2>
+						<div class="product_detail">
+							<table>
+								<tbody>
+									<tr>
+										<td>Артикул</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Опция</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Цвет</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Размер</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Вес</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Артикул</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Опция</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Цвет</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Размер</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Вес</td>
+										<td></td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</section>
+				<section class="about_store">
+					
+				</section>
+			</div>
+		</div>
+	</div>
+	<footer class="footer" role="contentinfo">
+		<div class="container">
+			<div class="logo_store">
+				<a href="#" class="footer__logo"><span>Эппл Shop</span></a>
+			</div>
+			<ul class="footer__menu-list">
+				<li class="footer__menu-item">
+					<div class="footer__menu-title">
+						О магазине
+					</div>
+					<ul class="footer__menu-inners">
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">О нас</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Наши цены</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Наши скидки</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Акции</a></li>
+					</ul>
+				</li>
+				<li class="footer__menu-item">
+					<div class="footer__menu-title">
+						Каталог
+					</div>	
+					<ul class="footer__menu-inners">
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Смартфоны</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Планшеты</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Компьютеры</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Аксессуары</a></li>
+					</ul>				
+				</li>
+				<li class="footer__menu-item">
+					<div class="footer__menu-title">
+						Контакты
+					</div>						
+					<ul class="footer__menu-inners">
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Телефоны</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Адреса</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Карта проезда</a></li>
+						<li class="footer_menu-option"><a href="#" class="sub_menu_link">Обратная связь</a></li>
+					</ul>				
+				</li>
+			</ul>
+			<div class="footer__social">
+				<ul class="footer__social-stores">
+					<li class="footer__social-store"><a href="#" class="footer__social-store-link">AppleStore</a></li>
+					<li class="footer__social-store-link"><a href="#" class="footer__social-store-link">PlayMarket</a></li>
+				</ul>
+				<ul class="footer__social-networks">
+					<li class="footer__social-network footer__social-network-vk"><a href="#" class="footer__social-link "><span>Вконтакте</span></a></li>
+					<li class="footer__social-network footer__social-network-fb"><a href="#" class="footer__social-link"><span>Facebook</span></a></li>
+					<li class="footer__social-network footer__social-network-tw"><a href="#" class="footer__social-link"><span>Twitter</span></a></li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
